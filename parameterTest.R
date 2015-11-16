4837336bdd9ab949e21b062fb62e445ae49cd7e0
spencerdhunt <spencer.d.hunt@gmail.com>
2015-11-16 15:04:30 -0800

    Edited some notes regarding Parameter values and Mixing

--- parameterTest.R
+++ parameterTest.R
@@ -10,7 +10,7 @@ d<-c(0.005,0.005,0.005) #death rate (boy, queer, girl) = birth rate
 # v<-c(0,0,0.3)		#vaccination rates (boy, queer, girl)
 # w<-c(0,0)			#rate of vaccination decay (boy, girl)
 # eps<-c(1-0.9		# protection by vaccine 0 is perfect, 1 is no protection
-vaccStart<-1500		# in days
+vaccStart<-1500		# in months (125 years)
 
 #List testing for solutions
 v.list<-list(
